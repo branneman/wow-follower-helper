6999c02b17b6f495fc6cfbcd8ed9e801aec351b2
Bran van der Meer <branmovic@gmail.com>
2015-02-16 00:12:18 +0100

    Added ability icons

--- main.lua
+++ main.lua
@@ -25,7 +25,23 @@ function printFollower(follower)
   if (not follower.isCollected or follower.status == GARRISON_FOLLOWER_INACTIVE or follower.status == GARRISON_FOLLOWER_WORKING) then
     return
   end
-  print(colorize(follower.quality, level(follower)) .. ' - ' .. follower.name)
+  local level = colorize(follower.quality, level(follower))
+  local abilities = getAbilityIconString(follower)
+  print(level .. ' - ' .. follower.name .. ' - ' .. abilities)
+end
+
+function getAbilityIconString(follower)
+  local iconString = ''
+  local abilities = C_Garrison.GetFollowerAbilities(follower.followerID)
+  for i = 1, #abilities do
+    local ability = abilities[i];
+    if (not ability.isTrait) then
+      for id, counter in pairs(ability.counters) do
+        iconString = iconString .. getIconString(counter.icon) .. ' '
+      end
+    end
+  end
+  return iconString
 end
 
 function level(follower)
@@ -37,4 +53,8 @@ end
 
 function colorize(quality, text)
   return ITEM_QUALITY_COLORS[quality].hex .. text .. FONT_COLOR_CODE_CLOSE
+end
+
+function getIconString(icon)
+  return string.format('\124T%s:0:0:2:0\124t', icon)
 end
