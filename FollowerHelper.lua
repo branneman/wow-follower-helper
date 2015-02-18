87b5d121cf2711f5e05eb5a8b5ce5351b031bf4d
Bran van der Meer <branmovic@gmail.com>
2015-02-18 21:44:47 +0100

    Fixed indentation

--- FollowerHelper.lua
+++ FollowerHelper.lua
@@ -2,29 +2,29 @@ SLASH_FollowerHelper1, SLASH_FollowerHelper2 = '/fh', '/followerhelper'
 
 function SlashCmdList.FollowerHelper(msg)
 
-  -- Grab all followers
-  local followers = C_Garrison.GetFollowers()
+    -- Grab all followers
+    local followers = C_Garrison.GetFollowers()
 
-  -- Filter out ususable followers and sort
-  followers = filter(followers, isUsableFollower)
-  followers = sort(followers, sortFollowers)
+    -- Filter out ususable followers and sort
+    followers = filter(followers, isUsableFollower)
+    followers = sort(followers, sortFollowers)
 
-  -- Decorate followers with stringify methods
-  followers = map(followers, abilityDecorator)
-  followers = map(followers, levelDecorator)
+    -- Decorate followers with stringify methods
+    followers = map(followers, abilityDecorator)
+    followers = map(followers, levelDecorator)
 
-  -- Concat stringified follower properties
-  local abilities = map(followers, fn('stringifyAbilities'))
-  local levels    = map(followers, fn('stringifyLevel'))
-  local names     = map(followers, get('name'))
+    -- Concat stringified follower properties
+    local abilities = map(followers, fn('stringifyAbilities'))
+    local levels = map(followers, fn('stringifyLevel'))
+    local names = map(followers, get('name'))
 
-  -- Add stringified follower properties to Ui
-  columnAbilities:SetText(join(abilities))
-  columnLevels:SetText(join(levels))
-  columnNames:SetText(join(names))
+    -- Add stringified follower properties to Ui
+    columnAbilities:SetText(join(abilities))
+    columnLevels:SetText(join(levels))
+    columnNames:SetText(join(names))
 
-  -- Resize frame to contents and show frame
-  scaleFrameToText(FH_Window, columnAbilities, 20)
-  FH_Window:Show();
+    -- Resize frame to contents and show frame
+    scaleFrameToText(FH_Window, columnAbilities, 20)
+    FH_Window:Show();
 
 end
